a174323115dfca4ec158928bd3e2e65c55de25a8
Maarten Vanlinthout <mvlintho@users.noreply.github.com>
2021-09-14 10:09:52 +0200

    Update inject.js
    div example

--- inject.js
+++ inject.js
@@ -1,9 +1,13 @@
-// this is the code which will be injected into a given page...
-
 (function() {
 
-window.history.pushState(null, null, window.location.href);
-window.onpopstate = function () {
-    window.history.go(1);
-};
+	// just place a div at top right
+	var div = document.createElement('div');
+	div.style.position = 'fixed';
+	div.style.top = 0;
+	div.style.right = 0;
+	div.textContent = 'Injected!';
+	document.body.appendChild(div);
+
+	alert('inserted self... giggity');
+
 })();
